08517a20ed655ce0d9c9151f5616a61f394d76ca
SadiqNahmetov <sadigfn@code.edu.az>
2022-11-22 01:57:41 +0400

    react intro cv

---
+++ cv/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Cv from './components/layout/Cv';
+import Home from './pages/Home';
+
+
+
+
+function App() {
+
+  return (
+
+    <div>
+      <Home />
+      <Cv/>
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ cv/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from '../components/layout/Navigation'
+
+function Home() {
+  return (
+    <div>
+       <Navigation/>
+       
+        
+    </div>
+  )
+}
+
+export default Home
